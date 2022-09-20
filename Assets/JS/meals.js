b06c63181c2f0f3a63afc7dc41a4051de57fd77e
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2022-09-20 21:21:57 +0800

    add pseudocode for JS

---
+++ Assets/JS/meals.js
@@ -0,0 +1,15 @@
+
+
+
+
+// When the user gets to meals page
+    // They can click into an input and type in an ingredient
+        // An event listener listening for the submit button
+// When the user types in an ingredient and presses submit
+    // A random meal card pops up on the screen
+        // for loop to create the elements for each part of recipe
+        // DOM manipulation to pull data from API
+    // the user can choose to save that recipe to local storage or
+        // function attached to submit button to save recipe to local storage
+    // Bonus: generate a different recipe
+        // button to generate new random recipe with same ingredient
